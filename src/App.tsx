bbce1f5802dc0f8f1eeefab0a0e8f10d1ec6f610
Yunho Choi <choiking10@naver.com>
2023-07-23 20:14:00 +0900

    feat: implement Members

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import Members from '@components/Members/Members';
 import * as S from './App.style';
 import SettingBar from '@components/SettingBar/SettingBar';
 import Teams from '@components/Teams/Teams';
+import { useState } from 'react';
 
 function App() {
+  const [members, setMembers] = useState<string[]>([]);
+
   return (
     <>
       <S.H1>🪄 RANDOM TEAM MATCHER</S.H1>
       <S.Layout>
         <S.LNB>
-          <Members />
+          <Members members={members} setMembers={setMembers} />
         </S.LNB>
         <S.Content>
           <SettingBar />
--- src/components/Members/Members.tsx
+++ src/components/Members/Members.tsx
@@ -1,5 +1,43 @@
-const Members = () => {
-  return <div></div>;
+import { ChangeEvent, FormEvent, useState } from 'react';
+import * as S from './Members.style';
+
+interface IProps {
+  members: string[];
+  setMembers: (members: string[]) => void;
+}
+
+const Members = ({ members, setMembers }: IProps) => {
+  const [member, setMember] = useState<string>('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setMembers([...members, member]);
+    setMember('');
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value: newMember } = event.target;
+    setMember(newMember);
+  };
+
+  return (
+    <S.MembersWrapper>
+      {members.length > 0 ? (
+        <S.Members>
+          {members.map((member, index) => (
+            <S.Member key={index}>{member}</S.Member>
+          ))}
+        </S.Members>
+      ) : (
+        <S.EmptyMembers>멤버를 추가해주세요.</S.EmptyMembers>
+      )}
+      <S.Form onSubmit={handleSubmit}>
+        <input type="text" value={member} onChange={handleChange} />
+        <button type="submit">추가</button>
+      </S.Form>
+    </S.MembersWrapper>
+  );
 };
 
 export default Members;
