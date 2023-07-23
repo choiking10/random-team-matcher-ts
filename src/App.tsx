b5a945d0811047670c92998500d03e8f7c2ac3d5
Yunho Choi <choiking10@naver.com>
2023-07-23 20:06:14 +0900

    feat: make layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import Members from '@components/Members/Members';
 import * as S from './App.style';
+import SettingBar from '@components/SettingBar/SettingBar';
+import Teams from '@components/Teams/Teams';
 
 function App() {
   return (
     <>
       <S.H1>🪄 RANDOM TEAM MATCHER</S.H1>
       <S.Layout>
-        <S.LNB></S.LNB>
-        <S.Content></S.Content>
+        <S.LNB>
+          <Members />
+        </S.LNB>
+        <S.Content>
+          <SettingBar />
+          <Teams />
+        </S.Content>
       </S.Layout>
     </>
   );
